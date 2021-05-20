07934d34c910278539667d70385cf7350918a658
Alper OZKAN <alper.ozkan.p@gmail.com>
2021-05-20 18:32:19 +0200

    Added ids to function

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,32 @@ fn main() {
     /*
      * People declaration
      */
-    let helen: Person = Person::new("Helen", "Glenn");
-    let george: Person = Person::new("George", "Pierce");
-    let rose: Person = Person::new("Tom", "Pierce");
-    let tom = Person::new("Tom", "Pierce");
-
-    let rosa: Person = Person::new("Rosa", "Trent");
-    let gregor: Person = Person::new("Gregor", "Pierce");
-
-    
-
-    /*
-     * Family manipulation
-     */
     let mut fam1: CloseFamily = CloseFamily::new("Glenn-Pierce");
+    let mut fam2: CloseFamily = CloseFamily::new("Trent-Pierce");
+    let helen: Person;
+    let george: Person;
+    let rose: Person;
+    let tom: Person;
+    let rosa: Person;
+    let gregor: Person;
+
+    unsafe {
+        helen = Person::new("Helen", "Glenn");
+        george = Person::new("George", "Pierce");
+        rose = Person::new("Tom", "Pierce");
+        tom = Person::new("Tom", "Pierce");
+        rosa = Person::new("Rosa", "Trent");
+        gregor = Person::new("Gregor", "Pierce");
+    }
     fam1.add_parent(helen);
     fam1.add_parent(george);
     fam1.add_child(rose);
     fam1.add_child(tom);
 
-    let mut fam2: CloseFamily = CloseFamily::new("Trent-Pierce");
     fam2.add_parent(rosa);
     fam2.add_parent(gregor);
     fam2.add_child(george);
+    
 
     println!("{}", fam1);
 }
--- src/model/person.rs
+++ src/model/person.rs
@@ -1,22 +1,31 @@
 use std::fmt;
 
+static mut person_id: u32 = 0;
+
 #[derive(Clone, Copy)]
 pub struct Person<'a> {
+    id: u32,
     pub lastname: &'a str,
     pub surname: &'a str,
 }
 
 impl<'a> Person<'a> {
-    pub fn new(lastname: &'a str, surname: &'a str) -> Person<'a> {
-        Person {
+    pub unsafe fn new(lastname: &'a str, surname: &'a str) -> Person<'a> {
+        person_id += 1;
+        return Person {
+            id: person_id,
             lastname: lastname,
             surname: surname,
-        }
+        };
     }
 }
 
 impl<'a> fmt::Display for Person<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "lastname: {} \nsurname: {}", self.lastname, self.surname)
+        writeln!(
+            f,
+            "id: {} \nlastname: {} \nsurname: {}",
+            self.id, self.lastname, self.surname
+        )
     }
 }
