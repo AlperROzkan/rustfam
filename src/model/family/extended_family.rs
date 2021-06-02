92f6cb1b49214f5a8a6c17dbc0113741d16289b3
Alper OZKAN <alper.ozkan.p@gmail.com>
2021-06-02 22:28:51 +0200

    Fixed hashmap in ExtendedFamily

--- src/model/family/extended_family.rs
+++ src/model/family/extended_family.rs
@@ -18,12 +18,31 @@ pub struct ExtendedFamily<'a> {
 }
 
 impl<'a> ExtendedFamily<'a> {
-    
+    /// Instanciates a new family
+    pub fn new(name: &'a str, families: HashMap<Pairing<Person<'a>>, Pairing<CloseFamily<'a>>>) -> Self { Self { name, families } }
+
     /**
     Add a family to the extended family, must specify a pairing.
      * self
-     * pairing : two linked people that are the link between two families
-     * family
+     * couple_pairing: two linked people that are the link between two families
+     * family_pairing:  
      */
-    pub fn new(name: &'a str, families: HashMap<Pairing<Person<'a>>, Pairing<CloseFamily<'a>>>) -> Self { Self { name, families } }
+    pub fn add_family(&mut self, couple_pairing: Pairing<Person<'a>>, family_pairing: Pairing<CloseFamily<'a>>) {
+        self.families.insert(couple_pairing, family_pairing);
+    }
+
+    /// Get a reference to the extended family's name.
+    pub fn name(&self) -> &&'a str {
+        &self.name
+    }
+
+    /// Set the extended family's name.
+    pub fn set_name(&mut self, name: &'a str) {
+        self.name = name;
+    }
+
+    /// Get a reference to the extended family's families.
+    pub fn families(&self) -> &HashMap<Pairing<Person<'a>>, Pairing<CloseFamily<'a>>> {
+        &self.families
+    }
 }
