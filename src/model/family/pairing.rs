1a1f071e188c8f960cc9cd9584a0f07ac0fe7423
Alper OZKAN <alper.ozkan.p@gmail.com>
2021-06-02 21:40:57 +0200

    Made Pairing Generic

--- src/model/family/pairing.rs
+++ src/model/family/pairing.rs
@@ -1,45 +1,42 @@
-use crate::model::person::Person;
 use serde::{Deserialize, Serialize};
 
 /**
- * A Pairing between two people. This struct is used as a Key in a Collection in Extended family.
- * - linked_person_1
- * - linked_person_2
+ * A Pairing between two things.
+ * - linked_1
+ * - linked_2
  */
 #[derive(Debug, Clone, Copy, Hash, Serialize, Deserialize, PartialEq, Eq)]
-pub struct Pairing<'a> {
+pub struct Pairing<T> {
     // Each attribute must be borrowed explicitely
-    #[serde(borrow)]
-    linked_person_1: Person<'a>,
-    #[serde(borrow)]
-    linked_person_2: Person<'a>,
+    linked_1: T,
+    linked_2: T,
 }
 
-impl<'a> Pairing<'a> {
-    pub fn new(linked_person_1: Person<'a>, linked_person_2: Person<'a>) -> Self {
+impl<T> Pairing<T> {
+    pub fn new(linked_1: T, linked_2: T) -> Self {
         Self {
-            linked_person_1,
-            linked_person_2,
+            linked_1,
+            linked_2,
         }
     }
 
     /// Get a reference to the pairing's linked person 1.
-    pub fn linked_person_1(&self) -> &Person<'a> {
-        &self.linked_person_1
+    pub fn linked_1(&self) -> &T {
+        &self.linked_1
     }
 
     /// Get a mutable reference to the pairing's linked person 1.
-    pub fn linked_person_1_mut(&mut self) -> &mut Person<'a> {
-        &mut self.linked_person_1
+    pub fn linked_1_mut(&mut self) -> &mut T {
+        &mut self.linked_1
     }
 
     /// Get a reference to the pairing's linked person 2.
-    pub fn linked_person_2(&self) -> &Person<'a> {
-        &self.linked_person_2
+    pub fn linked_2(&self) -> &T {
+        &self.linked_2
     }
 
     /// Get a mutable reference to the pairing's linked person 2.
-    pub fn linked_person_2_mut(&mut self) -> &mut Person<'a> {
-        &mut self.linked_person_2
+    pub fn linked_2_mut(&mut self) -> &mut T {
+        &mut self.linked_2
     }
 }
